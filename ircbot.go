9935bf4ad724c30a5fa89690203f88d483306a2d
Akshay Shekhervoldyman666@gmail.com <voldyman666@gmail.com>
2015-04-17 18:58:02 +0530

    added ircbot.go

---
+++ ircbot.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/sorcix/irc"
+	"github.com/voldyman/ircx"
+)
+
+type (
+	IRCBot struct {
+		channels []string
+		bot      *ircx.Bot
+		chEvents chan IRCMessageEvent
+	}
+
+	IRCMessageEvent struct {
+		Sender string
+		Text   string
+	}
+)
+
+func newIRCBot(server, nick string, channels []string) *IRCBot {
+	return &IRCBot{
+		channels: channels,
+		bot:      ircx.Classic(server, nick),
+		chEvents: make(chan IRCMessageEvent),
+	}
+}
+
+func (i *IRCBot) Start() error {
+	err := i.bot.Connect()
+	if err != nil {
+		return err
+	}
+
+	i.registerHandlers()
+	return nil
+}
+
+func (i *IRCBot) Loop() chan IRCMessageEvent {
+	go i.bot.CallbackLoop()
+
+	return i.chEvents
+}
+
+func (i *IRCBot) SendMessage(nick, msg string) {
+	msgBuf := bytes.NewBufferString("")
+
+	fmt.Fprintf(msgBuf, "<%s> %s", nick, msg)
+
+	i.bot.SendMessage(&irc.Message{
+		Command:  "PRIVMSG",
+		Params:   i.channels,
+		Trailing: msgBuf.String(),
+	})
+}
+
+func (i *IRCBot) registerHandlers() {
+	// IRC Ping Pong handler
+	i.bot.AddCallback(irc.PING, ircx.Callback{
+		Handler: ircx.HandlerFunc(pingHandler),
+	})
+
+	// IRC register handler
+	i.bot.AddCallback(irc.RPL_WELCOME, ircx.Callback{
+		Handler: ircx.HandlerFunc(i.registerConnect),
+	})
+
+}
+
+func (i *IRCBot) msgHandler(s ircx.Sender, m *irc.Message) {
+	ev := IRCMessageEvent{
+		Sender: m.Name,
+		Text:   m.Trailing,
+	}
+	i.chEvents <- ev
+}
+
+func (i *IRCBot) registerConnect(s ircx.Sender, m *irc.Message) {
+	s.Send(&irc.Message{
+		Command: irc.JOIN,
+		Params:  i.channels,
+	})
+
+}
+
+func pingHandler(s ircx.Sender, m *irc.Message) {
+	s.Send(&irc.Message{
+		Command:  irc.PONG,
+		Params:   m.Params,
+		Trailing: m.Trailing,
+	})
+}
